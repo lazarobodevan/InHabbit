1cab73d08014687243f47e607cf9c06ce5604946
Lázaro Bodevan <lazaro.maia@ufv.br>
2021-11-28 00:53:53 -0300

    Login screen for Android

---
+++ AppInHabit/InRabbit.App/InRabbit.App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using InRabbit.App.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InRabbit.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppInHabit/InRabbit.App/InRabbit.App/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias = "Roboto")]
+[assembly: ExportFont("Roboto-Light.ttf", Alias = "RobotoLight")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias = "RobotoMedium")]
